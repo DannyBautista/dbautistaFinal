59d3fb63b9a2c420702687aa7d4189f50bd059d9
Danny <dannybautista276@gmail.com>
2022-06-26 12:47:54 -0500

    Examen Final.

--- src/main/java/elp/edu/examendbbautista/security/CurrenUser.java
+++ src/main/java/elp/edu/examendbbautista/security/CurrenUser.java
@@ -1,12 +1,4 @@
 package elp.edu.examendbbautista.security;
 
-
-import java.lang.annotation.*;
-
-@Target({ElementType.PARAMETER, ElementType.TYPE})
-@Retention(RetentionPolicy.RUNTIME)
-@Documented
-@AuthenticationPrincipal
-public @interface  CurrentUser {
-
+public @interface CurrenUser {
 }
